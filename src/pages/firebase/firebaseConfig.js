604c70a8f52d1f6fc62832baad9c40398adae941
sabbir0029 <mdsabbirhossain0029@gmail.com>
2021-11-03 10:16:35 +0600

    add

---
+++ src/pages/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6RHzAC_kwIpNqkU-I6R3qtfEcRpoTqTo",
+  authDomain: "sd-travel.firebaseapp.com",
+  projectId: "sd-travel",
+  storageBucket: "sd-travel.appspot.com",
+  messagingSenderId: "853060800605",
+  appId: "1:853060800605:web:d6b224b42dc55b3967fdb4"
+};
+export default firebaseConfig;
